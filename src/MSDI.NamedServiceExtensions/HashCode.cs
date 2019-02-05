84399c7a0549d8e3adb5b254d26fe12b7de74ca8
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2019-02-05 21:23:27 +0100

    Multiframework Simple HashCode util for net472 for now.

---
+++ src/MSDI.NamedServiceExtensions/HashCode.cs
@@ -0,0 +1,27 @@
+﻿#if NET472
+namespace MSDI.NamedServiceExtensions
+{
+    public static class HashCode
+    {
+        private const int MagicNumber = 397;
+
+        public static int Combine(params object[] parts)
+        {
+            unchecked
+            {
+                if (parts.Length == 0)
+                {
+                    return 0;
+                }
+
+                var hashCode = parts[0]?.GetHashCode() ?? 0;
+                for (var i = 1; i < parts.Length; i++)
+                    hashCode = (hashCode * MagicNumber) ^ (parts[i]?.GetHashCode() ?? 0);
+
+                return hashCode;
+            }
+        }
+
+    }
+}
+#endif
